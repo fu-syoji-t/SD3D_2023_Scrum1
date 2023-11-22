6d29056129b3eccba638e61ab6aba87522c3d538
Shige0722 <117242960+Shige0722@users.noreply.github.com>
2023-11-22 11:34:41 +0900

    更新

---
+++ game_db/login.php
@@ -0,0 +1,18 @@
+<?php 
+session_start();
+?>
+<!DOCTYPE html>
+<html>
+
+<head>
+<meta content="text/html; charset=utf-8" http-equiv="Content-Type">
+<title>モンスターフレンズ | ログイン</title>
+</head>
+<body>
+<h2>ログイン</h2>
+メールアドレス:<input type="text" name=""><br>
+パスワード:<input type="password" name=""><br>
+名前:<input type="text" name=""><br>
+<input type="submit" value="ログイン">
+</body>
+</html>
--- game_db/ログイン.php
+++ game_db/ログイン.php
@@ -5,20 +5,35 @@ session_start();
 <html>
 
 <head>
+<meta charset="UTF-8">
+<meta http-equiv="Content-Script-Type" content="text/javascript">
+<script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+<link rel="stylesheet" href="style.css">
+<link href="login.css" rel="stylesheet" type="text/css"><link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.0/font/bootstrap-icons.css">
 <meta content="text/html; charset=utf-8" http-equiv="Content-Type">
-<title>モンスターフレンズ | ログイン</title>
+<title>ログイン</title>
 </head>
 <body>
 <h2>ユーザーログイン</h2>
+<body style="background-image: url('background/352_1.png');">
 <form action="index2.php"method="post">
 <!-- メールアドレス -->
-<input type="text" name="mail" required placeholder="メールアドレス"><br>
+<div class="magin30">
+<input type="text" name="mail" required placeholder="メールアドレス"style="border-width:3px;"><br>
+</div>
 <!-- パスワード -->
-<input type="password" name="password" required placeholder="パスワード"><br>
-<!-- 名前 -->
-<input type="text" name="name" required placeholder="名前"><br>
+<div class="magin30">
+<input type="password" name="password" required placeholder="パスワード"style="border-width:3px;"><br>
+<?php
+     if((isset($_SESSION['error']))){
+    echo '<div style="color: red; text-align: center;">'.$_SESSION['error'].'</div>';
+    unset($_SESSION['error']);
+        }
+?>
+</div>
 <button type="submit"class="login"style="background-color:#008080;"href="index2.php">ログイン</button>
 </form>
 <a class = "new" href="index2.php">新規登録</a>
+
 </body>
 </html>
