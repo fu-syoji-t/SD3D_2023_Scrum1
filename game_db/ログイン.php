f354534b2c82134117ee009f71eebc0daba9425c
Shige0722 <117242960+Shige0722@users.noreply.github.com>
2023-12-01 10:18:35 +0900

    更新

--- game_db/ログイン.php
+++ game_db/ログイン.php
@@ -1,3 +1,4 @@
+
 <?php 
 session_start();
 ?>
@@ -9,31 +10,63 @@ session_start();
 <meta http-equiv="Content-Script-Type" content="text/javascript">
 <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
 <link rel="stylesheet" href="style.css">
-<link href="login.css" rel="stylesheet" type="text/css"><link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.0/font/bootstrap-icons.css">
+<link href="login2.css" rel="stylesheet" type="text/css"><link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.0/font/bootstrap-icons.css">
 <meta content="text/html; charset=utf-8" http-equiv="Content-Type">
+<form method="POST"action="login.php">
 <title>ログイン</title>
 </head>
 <body>
-<h2>ユーザーログイン</h2>
-<body style="background-image: url('background/352_1.png');">
-<form action="index2.php"method="post">
-<!-- メールアドレス -->
-<div class="magin30">
-<input type="text" name="mail" required placeholder="メールアドレス"style="border-width:3px;"><br>
-</div>
-<!-- パスワード -->
-<div class="magin30">
-<input type="password" name="password" required placeholder="パスワード"style="border-width:3px;"><br>
-<?php
-     if((isset($_SESSION['error']))){
-    echo '<div style="color: red; text-align: center;">'.$_SESSION['error'].'</div>';
-    unset($_SESSION['error']);
-        }
-?>
-</div>
-<button type="submit"class="login"style="background-color:#008080;"href="index2.php">ログイン</button>
+<div class="login-wrap">
+  <div class="login-html">
+    <input id="tab-1" type="radio" name="tab" class="sign-in" checked><label for="tab-1" class="tab">Sign In</label>
+    <input id="tab-2" type="radio" name="tab" class="sign-up"><label for="tab-2" class="tab">Sign Up</label>
+    <div class="login-form">
+      <div class="sign-in-htm">
+        <div class="group">
+          <label for="user" class="label">UserMail</label>
+          <input id="user" type="text" class="input" name="mail">
+        </div>
+        <!-- パスワード -->
+        <div class="group">
+          <label for="pass" class="label">Password</label>
+          <input id="pass" type="password" class="input" name="password" data-type="password">
+        </div>
+        <div class="hr"></div>
+        <div class="group">
+        <input type="submit" class="button" value="Sign In">
+        </div>
 </form>
-<a class = "new" href="index2.php">新規登録</a>
-
+        
+<form method="POST"action="register.php">
+<title>新規登録</title>
+      </div>
+      <div class="sign-up-htm">
+        <div class="group">
+          <label for="user" class="label">Username</label>
+          <input id="user" type="text" class="input" name = "user">
+        </div>
+        <div class="group">
+          <label for="pass" class="label">Password</label>
+          <input id="pass" type="password" class="input" data-type="password" name = "pass">
+        </div>
+        <div class="group">
+          <label for="pass" class="label">Repeat Password</label>
+          <input id="pass" type="password" class="input" data-type="password" name = "pass">
+        </div>
+        <div class="group">
+          <label for="mail" class="label">Email Address</label>
+          <input id="mail" type="text" class="input" name = "mail">
+        </div>
+        <div class="hr"></div>
+        <div class="group">
+          <input type="submit" class="button" value="Sign Up">
+        </div>
+        <div class="foot-lnk">
+          <label for="tab-1">Already Member?</a>
+</form>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
 </body>
-</html>
