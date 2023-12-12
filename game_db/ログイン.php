7bc0fa491de19a9c9b8d77959b023d6d5e44f30b
Shige0722 <117242960+Shige0722@users.noreply.github.com>
2023-12-12 11:58:49 +0900

    更新

--- game_db/ログイン.php
+++ game_db/ログイン.php
@@ -1,5 +1,5 @@
 
-<?php 
+<?php
 session_start();
 ?>
 <!DOCTYPE html>
@@ -9,8 +9,7 @@ session_start();
 <meta charset="UTF-8">
 <meta http-equiv="Content-Script-Type" content="text/javascript">
 <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
-<link rel="stylesheet" href="style.css">
-<link href="login.css" rel="stylesheet" type="text/css"><link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.0/font/bootstrap-icons.css">
+<link href="login2.css" rel="stylesheet" type="text/css"><link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.0/font/bootstrap-icons.css">
 <meta content="text/html; charset=utf-8" http-equiv="Content-Type">
 <form method="POST"action="login.php">
 <title>ログイン</title>
@@ -23,14 +22,23 @@ session_start();
     <div class="login-form">
       <div class="sign-in-htm">
         <div class="group">
-          <label for="user" class="label"><font color="white">EMail address</font></label>
+          <label for="user" class="label">Email address</label>
           <input id="user" type="text" class="input" name="mail" required>
         </div>
         <!-- パスワード -->
         <div class="group">
-          <label for="pass" class="label"><font color="white">Password</font></label>
+          <label for="pass" class="label">Password</label>
           <input id="pass" type="password" class="input" name="password" data-type="password" required>
         </div>
+        <div>　</div>
+        <div>　</div>
+        <h4><font color="red">
+        <?php
+        if (isset($_GET['error'])) {
+          echo '<div class="error-message" >'. $_GET['error'] . '</div>';
+      }
+        ?>
+        </font></h4>
         <div class="hr"></div>
         <div class="group">
         <input type="submit" class="button" value="Sign In">
